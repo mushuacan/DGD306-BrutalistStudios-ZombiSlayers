ed7fc25a8c61361160563dffa80ce96d46ae8ab4
mushuacan <mushuacan@gmail.com>
2025-03-09 16:19:19 +0300

    2d animation tests

--- Zombi Slayers/Assets/Testing/Scripts/DummyMovement.cs
+++ Zombi Slayers/Assets/Testing/Scripts/DummyMovement.cs
@@ -12,6 +12,7 @@ public class CharacterMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isGrounded;
+    private int jumpPhase;
 
     void Start()
     {
@@ -24,18 +25,40 @@ public class CharacterMovement : MonoBehaviour
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-        animator.SetFloat("Horizontal Speed", rb.velocity.x);
+        animator.SetFloat("Horizontal Speed", Mathf.Abs(rb.velocity.x));
         animator.SetFloat("Vertical Speed", rb.velocity.y);
 
         // Zeminde olup olmadýðýný kontrol et
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
         animator.SetBool("IsGrounded", isGrounded);
 
-        // Zýplama iþlemi
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (isGrounded && Input.GetKey(KeyCode.Space) && (jumpPhase == 0 || jumpPhase == 1))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpPhase = 1;
             animator.SetBool("Jumping", true);
+        }
+        else if (isGrounded && !Input.GetKey(KeyCode.Space) && jumpPhase == 1)
+        {
+            jumpPhase = 2;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            animator.SetBool("Jumping", false);
+        }
+        else if (!isGrounded && jumpPhase == 2)
+        {
+            jumpPhase = 3;
+        }
+        else
+        {
+            jumpPhase = 0;
+        }
+
+        if (rb.velocity.x < 0) // Karakter sola bakacak
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // X ekseninde negatif yap
+        }
+        else if (rb.velocity.x > 0) // Karakter saða bakacak
+        {
+            transform.localScale = new Vector3(1, 1, 1); // X ekseninde pozitif yap
         }
     }
 }
