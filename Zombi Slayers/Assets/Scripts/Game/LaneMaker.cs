838941eb1f101f2c119131f232519a7bfb4792da
mushuacan <mushuacan@gmail.com>
2025-05-09 15:37:35 +0300

    v0.8.6.6 - Level katları ayarlandı

---
+++ Zombi Slayers/Assets/Scripts/Game/LaneMaker.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class LaneMaker : MonoBehaviour
+{
+    [SerializeField] private bool snap;
+    public int laneIndex;
+
+    private void OnValidate()
+    {
+        SnapToNearestLaneX();
+        snap = false;
+        if (snap) { /*_*/ }
+    }
+
+    void SnapToNearestLaneX()
+    {
+        transform.position = new Vector3(Mathf.Round(transform.position.x / 19.19f) * 19.19f, 0, 0);
+    }
+}
