fda719ef0b9e4d2910d92b0b379f994dac290b85
mushuacan <mushuacan@gmail.com>
2025-05-10 15:52:31 +0300

    v0.8.6.7 - Arrangements
     * Görsellerin filtresi değiştirildi
     * Oyun içi olmayan ama oyunu yapmayı kolaylaştıran kodlar için özel kod klasörü açıldı
     * Fletcher'ın mermileri ayarlandı
     * Fletcher'ın görseli ayarlandı.
     * Bazı eski görselleri yenileriyle değiştirildi.
     * Kutunun kırılınca 3-4 tane mühimmat çıkarmasına sebep olan hata yapay zekaya çözdürüldü
     * Karakter UI'nın can barı gibi etkenler düzenlendi (fletcher'ın büyüklüğünden dolayı)

--- Zombi Slayers/Assets/Scripts/Game/Box Breake.cs
+++ Zombi Slayers/Assets/Scripts/Game/Box Breake.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class BoxBreake : MonoBehaviour
 {
     public Scriptable_BoxInsiders insider;
+    private bool isTriggered = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (isTriggered) return; // Eðer zaten tetiklendiyse, iþlemi durdur
+
         if (collision.CompareTag("Player_Bullet"))
         {
-            Instantiate(insider.prefab, transform.position, transform.rotation, transform.parent);
+            isTriggered = true; // Tetiklendi olarak iþaretle
 
+            Instantiate(insider.prefab, transform.position, transform.rotation, transform.parent);
             Destroy(this.gameObject);
         }
     }
+
 }
