00a4f68cfeaf376f6eab6f7d7af673c65494f308
mushuacan <mushuacan@gmail.com>
2025-05-16 16:58:09 +0300

    v0.9.1.6 - Tutorials Added

---
+++ Zombi Slayers/Assets/Scripts/Menu/InGame/Tutorial Menu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMenu : MonoBehaviour
+{
+    private PlayerManager playerManager;
+    private Player_Inputs p1, p2;
+    private bool pressedAtBeggining;
+    void Start()
+    {
+        pressedAtBeggining = true;
+        Time.timeScale = 0.0f;
+        playerManager = FindAnyObjectByType<PlayerManager>();
+        if (playerManager != null )
+        {
+            p1 = playerManager.players[0].GetComponent<Player_Inputs>();
+            if (playerManager.playerCount == 2 )
+                p2 = playerManager.players[1].GetComponent<Player_Inputs>();
+        }
+        else
+        {
+            p1 = FindAnyObjectByType<Player_Inputs>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( pressedAtBeggining)
+        {
+            if (p2 == null)
+            {
+                if (!p1.button0pressed)
+                {
+                    pressedAtBeggining = false;
+                }
+            }
+            else
+            {
+                if (!p1.button0pressed && !p2.button0pressed)
+                {
+                    pressedAtBeggining = false;
+                }
+            }
+        }
+        else
+        {
+            if (p2 == null)
+            {
+                if (p1.button0pressed )
+                {
+                    Time.timeScale = 1.0f;
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                if (p1.button0pressed || p2.button0pressed)
+                {
+                    Time.timeScale = 1.0f;
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
