73a53343b09bb5e2d5043f1045b13dd118dd8194
mushuacan <mushuacan@gmail.com>
2025-05-17 04:15:24 +0300

    v0.9.1.10 - Patlayan zombi eklendi

---
+++ Zombi Slayers/Assets/Scripts/Game/Zombi/CreateExplosionWhileDying.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateExplosionWhileDying : MonoBehaviour
+{
+    [SerializeField] private GameObject _explosion;
+    [SerializeField] private bool _willExplode;
+    [SerializeField] private float _atXPosition;
+
+    private void Update()
+    {
+        if (_willExplode)
+        {
+            if (transform.position.x < _atXPosition)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(_explosion, this.transform.position, Quaternion.identity);
+    }
+}
