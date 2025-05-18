5c6bb8a71d176cd6f990716983272ebc222c4e1a
mushuacan <mushuacan@gmail.com>
2025-05-18 10:46:03 +0300

    v0.9.2.13 - Bugfix
    Artık Zombi Boss kameranın olmaması gereken bir yere gitmesine sebep olmamalı

--- Zombi Slayers/Assets/Scripts/Game/CameraShaker.cs
+++ Zombi Slayers/Assets/Scripts/Game/CameraShaker.cs
@@ -7,9 +7,21 @@ public class CameraShaker : MonoBehaviour
     public float strength = 0.3f;
     public int vibrato = 20;
     public float randomness = 90f;
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
 
     public void Shake()
     {
-        transform.DOShakePosition(duration, strength, vibrato, randomness, false, true);
+        transform.DOShakePosition(duration, strength, vibrato, randomness, false, true)
+                 .OnKill(() => ReturnToInitialPosition());
+    }
+
+    void ReturnToInitialPosition()
+    {
+        transform.DOMove(initialPosition, 0.01f);
     }
 }
