1dc0d817083eebb346feef085a811c6918648b5b
mushuacan <mushuacan@gmail.com>
2025-06-14 19:34:09 +0300

    v0.9.6.4.3 - ALL PREFABS SORTINGS LAYER ARRANGED

--- Zombi Slayers/Assets/Scripts/Game/Zombi/Zombi ATB/ZombiAtTheBack.cs
+++ Zombi Slayers/Assets/Scripts/Game/Zombi/Zombi ATB/ZombiAtTheBack.cs
@@ -4,11 +4,13 @@ using DG.Tweening;
 public class ZombiAtTheBack : MonoBehaviour
 {
     public float spaceBetweenZombis;
+    public SpriteRenderer resim;
     private float[] laneYPoz = { -24f, -4.12f, -1.08f, 1.96f };
 
     public void SetPosition(int lane, int order, float lanerXPosition)
     {
         float orderPosition = lanerXPosition - order * spaceBetweenZombis;
         transform.position = new Vector3(orderPosition, laneYPoz[lane], 0);
+        resim.sortingOrder = 10 + order;
     }
 }
